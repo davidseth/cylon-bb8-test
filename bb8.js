15103b862107034a54248cc109724f46ece978fd
David Peterson <david@davidseth.net>
2015-10-25 12:42:40 +1000

    this actually works from the web console!!! yay! it has to do with function scope, etc.

--- bb8.js
+++ bb8.js
@@ -35,69 +35,113 @@ Cylon.robot({
     devices: {
         bb8: {driver: 'bb8'}
     },
+    events: ['heading', 'turned_off'],
     sayRelax: function() {
-      return this.name + " says relax";
+        return this.name + " says relax";
     },
-    roll: function() {
-      console.log("Rolling!");
-      this.bb8.setRGB(0xFF0000, 0, this.callbackFn);
-      //this.sphero.roll(60, Math.floor(Math.random() * 360));
+    roll: function(my) {
+        console.log("Rolling!");
+        console.log(this);
+        var david = this;
+        this.bb8.devModeOn(function(wakeError) {
+            console.log("wake", wakeError);
+            console.log("----------------------------------");
+            console.log(david);
+            //this.bb8.getDeviceMode(function(dvcModeError) {
+            //    console.log("getDeviceMode", dvcModeError);
+            //});
+            david.bb8.roll(180, 180, 1);
+
+            //setTimeout(function(david) { console.log("roll");  david.bb8.roll(180, 180, 1, callbackFn); }, 20);
+        });
+    },
+    roll2: function(my) {
+        console.log("Rolling 2");
+        var david = this;
+        console.log(this);
+        //this.bb8.wake
+        this.bb8.roll(180, 90, 1);
+
+    },
+    ollie: function(my) {
+        var david = this;
+        this.bb8.wake(function(err, data){
+            console.log("wake");
+            after(200, function() {
+                david.bb8.setRGB(0x00FFFF);
+            });
+            after(500, function() {
+                david.bb8.setRGB(0xFF0000);
+
+                david.bb8.roll(60, 0, 1);
+                after(1000, function(){
+                    david.bb8.roll(60, 90, 1);
+
+                    after(1000, function(){
+                        david.bb8.stop();
+                    });
+                });
+            });
+        });
     },
     init: function() {
-      console.log('starting ...');
-          this.bb8.getDeviceMode(function(dvcModeError) {
-             console.log("getDeviceMode", dvcModeError);
-          });
+        console.log('starting ...');
+        this.bb8.getDeviceMode(function(dvcModeError) {
+            console.log("getDeviceMode", dvcModeError);
+        });
 
 
 
-          this.bb8.getRGB(function(dvcModeError) {
-              console.log("getRGB", dvcModeError);
-          });
+        this.bb8.getRGB(function(dvcModeError) {
+            console.log("getRGB", dvcModeError);
+        });
 
-      // this.bb8.getDeviceMode(function(dvcModeError) {
-      //    console.log("getDeviceMode", dvcModeError);
-      // });
+        // this.bb8.getDeviceMode(function(dvcModeError) {
+        //    console.log("getDeviceMode", dvcModeError);
+        // });
     },
     work: function(my) {
-      var callbackFn = function(cbError) {
-        console.log('called');
-          if (cbError) {
-              console.log("callbackFn", cbError);
-          }
-      };
-      //my.init();
+        var callbackFn = function(cbError) {
+            console.log('called');
+            if (cbError) {
+                console.log("callbackFn", cbError);
+            }
+        };
+        //my.init();
 
-      // this.bb8.getDeviceMode(function(dvcModeError) {
-      //    console.log("getDeviceMode", dvcModeError);
-      //  });
-      // this.bb8.getRGB(function(dvcModeError) {
-      //     console.log("getRGB", dvcModeError);
-      // });
+        // this.bb8.getDeviceMode(function(dvcModeError) {
+        //    console.log("getDeviceMode", dvcModeError);
+        //  });
+        // this.bb8.getRGB(function(dvcModeError) {
+        //     console.log("getRGB", dvcModeError);
+        // });
 
-      // my.bb8.roll(80, 0, 1, callbackFn);
-      // my.bb8.setRGB(0xFF0000, 0, callbackFn);
+        // my.bb8.roll(80, 0, 1, callbackFn);
+        // my.bb8.setRGB(0xFF0000, 0, callbackFn);
 
-      // every((1).second(), function() {
-      //   //this.roll();
-      //   console.log(my.sayRelax());
-      //   my.roll();
-      //   //my.bb8.roll(180, 0, 1, callbackFn);
-      //   //my.bb8.setRGB(0xFF0000, 0, callbackFn);
-      // });
+        every((1).second(), function() {
+            console.log(my.bb8.heading);
+            my.emit('heading', { data: my.bb8.heading});
+        });
     },
-
-    commands: function() {
-      var commands = {};
-
-      commands.init = this.init
-      commands.do_a_thing = this.doAThing
-
-      return commands;
+    commands: {
+        init: function() {this.init.call(this); },
+        roll: function() {this.roll.call(this);},
+        roll2: function() {this.roll2.call(this);},
+        ollie: function() {this.ollie.call(this);}
     },
+    //commands: function() {
+    //  var commands = {};
+    //
+    //  commands.init = this.init
+    //  commands.do_a_thing = this.doAThing
+    //  commands.roll = this.roll
+    //
+    //  return commands;
+    //},
 
     doAThing: function() {
-      console.log("I did a thing!");
+        console.log("I did a thing!");
     }
 });
 
